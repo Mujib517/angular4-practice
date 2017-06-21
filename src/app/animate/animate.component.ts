551a8719b25377ec77b93b35b3756d43ff9e1b9e
rahmam <mohammed.rahman@cdk.com>
2017-06-21 17:12:06 +0530

    Animations bootstrap hot and cold observables

--- src/app/animate/animate.component.ts
+++ src/app/animate/animate.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, animate, transition, style } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-animate',
@@ -7,25 +9,63 @@ import { trigger, state, animate, transition, style } from '@angular/core';
 
 <button class="btn btn-primary" (click)="animate()">Animate</button>
 
+<button class="btn btn-primary" (click)="resolve()">Reload</button>
+
     <p [@visibilityChanged]="visible">
       animate Works!
     </p>
+
+
+   <div class="col-sm-3">
+      <div class="well" *ngFor="let user of users" [@visibilityChanged]="visible">
+         <h4>{{user.login}}</h4>
+         <hr>
+         <div>
+            <img [src]="user.avatar_url" class="img-circle" width="200" height="200"/>
+         </div>
+      </div>
+   </div>
+    
   `,
-  animations:[
-    trigger('visibilityChanged',[
-      state('true',style({opacity:1})),
-      state('false',style({opacity:0})),
-      transition('*=>*',animate('0.5s'))
+  animations: [
+    trigger('visibilityChanged', [
+      state('true', style({ opacity: 1 })),
+      state('false', style({ opacity: 0 })),
+      transition("*=>*", animate(400))
     ])
   ]
 })
 export class AnimateComponent {
 
   visible = false;
+  users: any[];
+  obs: any;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   animate() {
     this.visible = !this.visible;
+  }
+
+  ngOnInit() {
+
+    //http observable is cold observable. Wont make a call unless subscribed.
+    this.obs = this.http.get('https://api.github.com/users')
+      .map(response => response.json())
+      .subscribe((data) => {
+        this.users = data;
+        this.visible = true;
+      })
+    //.toPromise();
+
+
+    //converting an observable to promise will not make extra call to http rather resolves the same promise
+
+    //this.obs.subscribe(data => this.users = data);
+  }
+
+  resolve() {
+    this.obs.subscribe(data => this.users = data);
+    //cold observable will make an http call each time we subscribe to it.
   }
 }
